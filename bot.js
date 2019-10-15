3040bdb4a7438652e09770789bd694188355998b
GitSquiddo <55364481+GitSquiddo@users.noreply.github.com>
2019-10-15 16:23:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -186,7 +186,7 @@ var bio = {};
 		message.replace('Sorry, can\'t load anything.');
 		break;
             case 'setBio':
-		bio[message.author.id] = args[1];
+		bio[message.author.id] = args[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 		message.channel.send('Your bio has been changed!')
 	        .then(msg => msg.delete(5000)); 
 		break;
